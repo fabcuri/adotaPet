c5656b9a4098fcaad48edfdf1379aea4218fba16
Fabcuri <fabcuri70@gmail.com>
2024-08-14 21:54:12 -0300

    Validando os campos e criando objeto no js

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,4 +1,4 @@
-function cadastrar(event){
+function cadastrar(event) {
     event.preventDefault()
 
     const foto = document.getElementById('foto').value
@@ -7,14 +7,45 @@ function cadastrar(event){
     const cor = document.getElementById('cor').value
     const descricao = document.getElementById('descricao').value
     const tipo = document.getElementById('tipo').value
-    
-    if(nome === ""){
-        console.log("oi")
-        //alert("Nome é obrigatório")
+
+    if (foto === "") {
+        document.getElementById('foto').style.borderColor = "red"
+        document.getElementById('error-foto').innerText = 'Foto é obrigatoria'
+    }else{
+         document.getElementById('foto').style.borderColor = ""
+        document.getElementById('error-foto').innerText = ""
+    }
+
+    if (!nome) { //veja uma outra forma de declarar a variavel vazia
         document.getElementById('nome').style.borderColor = "red"
         document.getElementById('error-nome').innerText = 'Nome é obrigatorio'
-    }
-    
+    }else{
+        document.getElementById('nome').style.borderColor = ""
+       document.getElementById('error-nome').innerText = ""
+   }
+
+    if (cor === "") {
+        document.getElementById('cor').style.borderColor = "red"
+        document.getElementById('error-cor').innerText = 'Cor é obrigatoria'
+    }else{
+        document.getElementById('cor').style.borderColor = ""
+       document.getElementById('error-cor').innerText = ""
+   }
+    if (descricao === "") {
+        document.getElementById('descricao').style.borderColor = "red"
+        document.getElementById('error-descricao').innerText = 'Descrição  é obrigatoria'
+    }else{
+        document.getElementById('descricao').style.borderColor = ""
+       document.getElementById('error-descricao').innerText = ""
+   }
+    if (tipo === "") {
+        document.getElementById('tipo').style.borderColor = "red"
+        document.getElementById('error-tipo').innerText = 'Tipo é obrigatorio'
+    }else{
+        document.getElementById('tipo').style.borderColor = ""
+       document.getElementById('error-tipo').innerText = ""
+   }
+
 
     const pet = {
         foto: foto,
@@ -24,18 +55,20 @@ function cadastrar(event){
         descricao: descricao,
         tipo: tipo
     }
-    // // localStorage.setItem("perfil",JSON.stringify(pet))
+    const listaNoLocalStorage = JSON.parse(localStorage.getItem("pets"))|| []//vai no localstorage e pegua a lista
+//se o valor for nulo...pq nao tem informação ainda....ele vai atribuir um array vazio
+    listaNoLocalStorage.push(pet)
 
-    console.log(pet)
-    
+    localStorage.setItem("pets",JSON.stringify(listaNoLocalStorage)) //esse comando salva a informação em string no localstorage
+
 }
 
 document
-.getElementById('form-pet')
-.addEventListener('submit', cadastrar )
+    .getElementById('form-pet')
+    .addEventListener('submit', cadastrar)
 
 // function alterarImagem(event) {
-//     const valorUrl = event.srcElement.value 
+//     const valorUrl = event.srcElement.value
 //     document.getElementById("catImagePreview").src = valorUrl
 //     document.getElementById("catImagePreview").style.borderRadius = "50%"
 // }
